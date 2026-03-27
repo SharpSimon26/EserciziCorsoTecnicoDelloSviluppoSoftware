e997733d11957052f70b6d4ed9fd8fca7a2418e9
Simone Castellaneta <simone.castellaneta@ialedu.it>
2026-03-27 13:06:18 +0100

    nuovo esercizio calcolatrice

--- 20260327-cs/ConsAppAlberello/ConsAppAlberello/Program.cs
+++ 20260327-cs/ConsAppAlberello/ConsAppAlberello/Program.cs
@@ -1,5 +1,22 @@
-﻿var dimensione = 16;
+﻿Console.Write("Inserisci la dimensione dell'albero: ");
+if (!int.TryParse(Console.ReadLine(), out int dimensione) && dimensione > 0)
+{
+    Console.WriteLine("Errore: è necessario un numero intero positivo");
+    return;
+}
+
 string riga;
+
+// V0
+var step = 1;
+for (int i = 1; i <= dimensione * 2; i += 2)
+{
+    Console.Write(new string('_', dimensione - step));
+    step++;
+    Console.WriteLine(new string('x', i));
+}
+
+Console.WriteLine();
 
 // V1
 for (int i = 1; i <= dimensione; i++)
@@ -11,8 +28,10 @@ for (int i = 1; i <= dimensione; i++)
 Console.WriteLine();
 
 // V2
-for (int i = 1; i <= dimensione * 2; i+=2)
+for (int i = 1; i <= dimensione * 2; i += 2)
 {
     riga = new string('*', i).PadLeft(dimensione + ((i + 1) / 2));
     Console.WriteLine(riga);
 }
+
+Console.ReadLine();
---
+++ 20260327-cs/ConsAppAlberello/ConsAppCalcolatrice/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Inserisci primo numero: ");
+if (!decimal.TryParse(Console.ReadLine(), out decimal num1))
+{
+    Console.WriteLine("Errore: è richiesto un numero intero");
+    Console.ReadLine();
+    return;
+}
+
+Console.Write("Inserisci secondo numero: ");
+if (!decimal.TryParse(Console.ReadLine(), out decimal num2))
+{
+    Console.WriteLine("Errore: è richiesto un numero intero");
+    Console.ReadLine();
+    return;
+}
+
+Console.Write("Quale operazione vuoi effettuare (+, -, *, /): ");
+string? operazione = Console.ReadLine();
+if (operazione == null)
+{
+    Console.WriteLine("Errore: definisci l'operazione da eseguire");
+    Console.ReadLine();
+    return;
+}
+
+decimal? risultato = null;
+
+switch (operazione)
+{
+    case "+":
+        risultato = num1 + num2;
+        break;
+    case "-":
+        risultato = num1 - num2;
+        break;
+    case "*":
+        risultato = num1 * num2;
+        break;
+    case "/":
+        risultato = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Errore: Operazione non riconosciuta");
+        Console.ReadLine();
+        return;
+}
+
+Console.WriteLine($"Il risultato è: {risultato}");
+Console.ReadLine();
