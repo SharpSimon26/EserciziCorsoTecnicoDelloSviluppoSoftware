7b86713097eb7872700e495dd0186a959fe04eb5
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2026-03-29 23:59:58 +0200

    Fix V0

--- 20260327-cs/EserciziConsole/ConsAppAlberello/Program.cs
+++ 20260327-cs/EserciziConsole/ConsAppAlberello/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Inserisci la dimensione dell'albero: ");
+﻿using System.Globalization;
+
+Console.Write("Inserisci la dimensione dell'albero: ");
 if (!int.TryParse(Console.ReadLine(), out int dimensione) && dimensione > 0)
 {
     Console.WriteLine("Errore: è necessario un numero intero positivo");
@@ -8,12 +10,21 @@ if (!int.TryParse(Console.ReadLine(), out int dimensione) && dimensione > 0)
 string riga;
 
 // V0
-var step = 1;
 for (int i = 1; i <= dimensione * 2; i += 2)
 {
-    Console.Write(new string('_', dimensione - step));
-    step++;
-    Console.WriteLine(new string('x', i));
+    //Console.Write(new string('_', dimensione - ((i + 1) / 2)));
+    for (int j = 1; j <= dimensione - ((i + 1) / 2); j++)
+    {
+        Console.Write(" ");
+    }
+
+    // Console.WriteLine(new string('*', i));
+    for (int k = 1; k <= i; k++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
 }
 
 Console.WriteLine();
@@ -30,7 +41,7 @@ Console.WriteLine();
 // V2
 for (int i = 1; i <= dimensione * 2; i += 2)
 {
-    riga = new string('*', i).PadLeft(dimensione + ((i + 1) / 2));
+    riga = new string('*', i).PadLeft(dimensione + (i / 2));
     Console.WriteLine(riga);
 }
 
