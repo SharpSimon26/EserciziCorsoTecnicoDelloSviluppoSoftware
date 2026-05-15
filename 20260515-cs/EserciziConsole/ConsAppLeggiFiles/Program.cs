58d3279068f78bf28ca33e696e1a7a1ebd2fbea5
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2026-05-15 10:56:18 +0200

    Esercizio console lettura file multitask

---
+++ 20260515-cs/EserciziConsole/ConsAppLeggiFiles/Program.cs
@@ -0,0 +1,32 @@
+﻿var folderPath = "Contents";
+var currentDir = Directory.GetCurrentDirectory();
+var files = Directory.GetFiles(Path.Join(currentDir, folderPath), "*.txt");
+
+if (files.Any())
+{
+    var fileLines = new Dictionary<string, int>();
+
+    await Task.WhenAll(files.Select(async filePath =>
+    {
+        var fileName = Path.GetFileName(filePath);
+
+        try
+        {
+            var numLines = (await File.ReadAllLinesAsync(filePath)).Length;
+            fileLines.TryAdd(fileName, numLines);
+        }
+        catch (Exception)
+        {
+            fileLines.TryAdd(fileName, -1);
+        }
+    }));
+
+    foreach (var item in fileLines)
+    {
+        Console.WriteLine(item.Key + " - " + item.Value + " lines");
+    }
+}
+else
+{
+    Console.WriteLine("La cartella non contiene files .txt");
+}
