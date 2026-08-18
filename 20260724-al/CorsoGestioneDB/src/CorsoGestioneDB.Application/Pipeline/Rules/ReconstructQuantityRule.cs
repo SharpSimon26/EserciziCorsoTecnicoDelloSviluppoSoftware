8e454c02be83c38563fda4348f2807965b888013
PySimon25 <213518354+PySimon25@users.noreply.github.com>
2026-08-19 01:24:21 +0200

    Modifiche a regole ricostruzione e test progetto ETL

--- 20260724-al/CorsoGestioneDB/src/CorsoGestioneDB.Application/Pipeline/Rules/ReconstructQuantityRule.cs
+++ 20260724-al/CorsoGestioneDB/src/CorsoGestioneDB.Application/Pipeline/Rules/ReconstructQuantityRule.cs
@@ -1,9 +1,17 @@
 using CorsoGestioneDB.Application.Engine;
+using Microsoft.Extensions.Logging;
 
 namespace CorsoGestioneDB.Application.Pipeline.Rules;
 
 public class ReconstructQuantityRule : IReconstructionRule
 {
+    private readonly ILogger<ReconstructQuantityRule> _logger;
+
+    public ReconstructQuantityRule(ILogger<ReconstructQuantityRule> logger)
+    {
+        _logger = logger;
+    }
+
     public bool CanApply(ImportContext context)
     {
         var line = context.Data.OrderLine;
@@ -17,12 +25,21 @@ public class ReconstructQuantityRule : IReconstructionRule
     public async Task ApplyAsync(ImportContext context)
     {
         var line = context.Data.OrderLine;
-        var calculatedQuantity = (line.Revenue - line.ShippingCost) / (line.UnitPrice * (1 - line.DiscountPct / 100m));
+        var revenue = line.Revenue.GetValueOrDefault();
+        var shippingCost = line.ShippingCost.GetValueOrDefault();
+        var unitPrice = line.UnitPrice.GetValueOrDefault();
+        var discountPct = line.DiscountPct.GetValueOrDefault();
+        var netUnitPrice = unitPrice * (1 - (discountPct / 100m)); // prezzo unitario scontato
 
-        if (calculatedQuantity == Math.Truncate(calculatedQuantity.GetValueOrDefault()))
-        {
-            line.Quantity = (int)calculatedQuantity;
-            context.Messages.Add(string.Format("Quantity modificato in {0} valore originale {1}", calculatedQuantity, line.Quantity));
-        }
+        var calculatedQuantity = (int)Math.Round(
+            (revenue - shippingCost) / netUnitPrice, 0, MidpointRounding.AwayFromZero
+        );
+
+        var msg = string.Format("Quantity modificato in {0} valore originale {1}", calculatedQuantity, line.Quantity);
+        context.Messages.Add(msg);
+        _logger.LogInformation("Ordine: {0} campo {1}", context.Data.Order.OrderID, msg);
+
+        line.Quantity = calculatedQuantity;
+
     }
 }
--- 20260724-al/CorsoGestioneDB/src/CorsoGestioneDB.Application/Pipeline/Rules/ReconstructUnitPriceRule.cs
+++ 20260724-al/CorsoGestioneDB/src/CorsoGestioneDB.Application/Pipeline/Rules/ReconstructUnitPriceRule.cs
@@ -1,9 +1,17 @@
 using CorsoGestioneDB.Application.Engine;
+using Microsoft.Extensions.Logging;
 
 namespace CorsoGestioneDB.Application.Pipeline.Rules;
 
 public class ReconstructUnitPriceRule : IReconstructionRule
 {
+    private readonly ILogger<ReconstructUnitPriceRule> _logger;
+
+    public ReconstructUnitPriceRule(ILogger<ReconstructUnitPriceRule> logger)
+    {
+        _logger = logger;
+    }
+
     public bool CanApply(ImportContext context)
     {
         var line = context.Data.OrderLine;
@@ -11,15 +19,26 @@ public class ReconstructUnitPriceRule : IReconstructionRule
         return line.Revenue.HasValue && line.Revenue > 0 &&
                line.Quantity.HasValue && line.Quantity > 0 &&
                line.UnitPrice.HasValue && line.UnitPrice <= 0 &&
-               line.DiscountPct.HasValue && line.DiscountPct >= 0;
+               line.DiscountPct.HasValue && line.DiscountPct >= 0 &&
+               line.ShippingCost.HasValue && line.ShippingCost >=0;
     }
     
     public async Task ApplyAsync(ImportContext context)
     {
         var line = context.Data.OrderLine;
-        //line.Revenue = line.Quantity * line.UnitPrice * (1 - line.DiscountPct / 100m);
-        var calculatedUnitPrice = (line.Revenue - line.ShippingCost) / (line.Quantity * (1 - line.DiscountPct / 100m));
-        context.Messages.Add(string.Format("UnitPrice modificato in {0} valore originale {1}", calculatedUnitPrice, line.UnitPrice));
+        var revenue = line.Revenue.GetValueOrDefault();
+        var shippingCost = line.ShippingCost.GetValueOrDefault();
+        var quantity = line.Quantity.GetValueOrDefault();
+        var discountPct = line.DiscountPct.GetValueOrDefault();
+        var discountFactor = quantity * (1 - (discountPct / 100m)); // quantità di articoli se comprati a prezzo pieno
+
+        var calculatedUnitPrice = Math.Round(
+            (revenue - shippingCost) / discountFactor, 2, MidpointRounding.AwayFromZero
+        );
+
+        var msg = string.Format("UnitPrice modificato in {0} valore originale {1}", calculatedUnitPrice, line.UnitPrice);
+        context.Messages.Add(msg);
+        _logger.LogInformation("Ordine: {0} campo {1}", context.Data.Order.OrderID, msg);
 
         line.UnitPrice = calculatedUnitPrice;
     }
